56f1678bc7f2d877ab8e81c8768626027de4554d
ferry237 <youbiferry@gmail.com>
2024-02-24 11:56:08 +0100

    session_3

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+tailwind.config = {
+    theme: {
+      extend: {
+
+        fontFamily:{
+            
+            roboto: ['"Roboto"', 'sans-serif;']
+         },
+        fontSize: {
+            'sm': '0.625rem', // 6.25px
+            'base': '1rem',   // 10px
+            'lg': '1.25rem',  // 12.5px
+            
+          },
+        colors: {
+          clifford: '#da373d',
+          Amber900:'#78350F',
+          Amber100:'#FEF3C7',
+          bgcolor100:'#FFFBEB',
+          jaune_ch:'#FBBF24',
+          bleu_triangle:'#0369A1',
+          session3bg:'#0F172A',
+          session3text:'#E2E8F0',
+
+               }
+
+      }
+    }
+  }
