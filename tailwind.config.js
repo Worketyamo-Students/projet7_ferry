caff640163e5c192b6200abf129d9f1d6ecd7a59
ferry237 <youbiferry@gmail.com>
2024-02-25 13:06:24 +0100

    commit2

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,15 @@ tailwind.config = {
           session3bg:'#0F172A',
           session3text:'#E2E8F0',
 
-               }
+               },
+         dropShadow: {
+                'md': '0 0 10px #fafaf6c3',
+                '4xl': [
+                    '0 35px 35px rgba(0, 0, 0, 0.25)',
+                    '0 45px 65px rgba(0, 0, 0, 0.15)'
+                ]
+              }
+               
 
       }
     }
